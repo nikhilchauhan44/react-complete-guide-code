ef233b9a79ca959746b484e84fddfc302682ea59
Nikhil Chauhan <nikhil.chauhan@tailnode.com>
2023-04-22 21:56:26 +0530

    completed food order app

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,15 +1,26 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-      <Header>Let's get started!</Header>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}>Let's get started!</Header>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
