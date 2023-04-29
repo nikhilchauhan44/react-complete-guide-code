bb7607bc0fd1ba72ff1fbb75c9c47eee343bb9e9
Nikhil Chauhan <nikhil.chauhan@tailnode.com>
2023-04-29 21:27:27 +0530

    done till 16

---
+++ code/01-starting-project/src/pages/Products.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <Link to="/products/p1">
+          <li>A Book</li>
+        </Link>
+        <Link to="/products/p2">
+          <li>A Carpet</li>
+        </Link>
+        <Link to="/products/p3">
+          <li>An Online Course</li>
+        </Link>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ code/09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -0,0 +1,23 @@
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q1",
+    author: "Nikhil",
+    text: "Learning React is great!",
+  },
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+];
+
+const AllQuotes = () => {
+  return <h1>All Quotes Page</h1>;
+};
+
+export default AllQuotes;
